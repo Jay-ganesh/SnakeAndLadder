c63d55c3768bd653f74dc66a0c348024958e30d8
Jay-ganesh <132564206+Jay-ganesh@users.noreply.github.com>
2024-12-02 11:30:04 +0530

    Single player position

---
+++ src/SnakeAndLadders/SnakeAndLadders.java
@@ -0,0 +1,10 @@
+package SnakeAndLadders;
+
+public class SnakeAndLadders {
+    public static void main(String[] args) {
+        int position = 0;
+
+        System.out.println("Welcome to Snake and Ladder Simulator.");
+        System.out.println("Single player position : "+position);
+    }
+}
