a63dbc9ac770e26ea61c176fb0d3823e6b028dba
Jay-ganesh <132564206+Jay-ganesh@users.noreply.github.com>
2024-12-02 12:09:24 +0530

    Repeating the player until end

--- src/SnakeAndLadders/SnakeAndLadders.java
+++ src/SnakeAndLadders/SnakeAndLadders.java
@@ -7,24 +7,37 @@ public class SnakeAndLadders {
         System.out.println("Welcome to Snake and Ladder Simulator.");
         Random random = new Random();
         int position = 0;
-        int rollDie = random.nextInt(6)+1;
-        int option = random.nextInt(3)+1;
+        int roll = 0;
 
-        switch (option){
-            case 1:
-                System.out.println("Option: No Play. The player stays at position : "+ position);
-                break;
-            case 2:
-                position += rollDie;
-                System.out.println("Option: Ladder! The player moves ahead to position " + position);
-                break;
-            case 3:
-                position -= rollDie;
-                if (position < 0) position = 0;
-                System.out.println("Option: Snake! The player moves back to position " + position);
-                break;
-            default:
-                System.out.println("Invalid..!");
+        while (position < 100) {
+            roll++;
+            int rollDie = random.nextInt(6) + 1;
+            int option = random.nextInt(3) + 1;
+
+            switch (option) {
+                case 1:
+                    System.out.println("Option: No Play. The player stays at position : " + position);
+                    break;
+                case 2:
+                    if (position + rollDie <= 100) { // Ensure player does not exceed 100
+                        position += rollDie;
+                        System.out.println("Option: Ladder! The player moves ahead to position " + position);
+                    } else {
+                        System.out.println("Option: Ladder! But you can't move beyond 100. Stay at " + position);
+                    }
+                    break;
+                case 3:
+                    position -= rollDie;
+                    if (position < 0) {
+                        position = 0;
+                    }
+
+                    System.out.println("Option: Snake! The player moves back to position " + position);
+                    break;
+                default:
+                    System.out.println("Invalid..!");
+            }
         }
+        System.out.println("\nCongratulations! The player reached position 100 in " + roll + " rolls.");
     }
 }
