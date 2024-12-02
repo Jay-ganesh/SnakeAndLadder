4f9e882a2dadf09875afe917dce5787fe4efe941
Jay-ganesh <132564206+Jay-ganesh@users.noreply.github.com>
2024-12-02 11:55:27 +0530

    Adding ladders and snakes:

--- src/SnakeAndLadders/SnakeAndLadders.java
+++ src/SnakeAndLadders/SnakeAndLadders.java
@@ -8,7 +8,23 @@ public class SnakeAndLadders {
         Random random = new Random();
         int position = 0;
         int rollDie = random.nextInt(6)+1;
-        System.out.println("Player is at position : "+position);
-        System.out.println("Player rolled : "+rollDie);
+        int option = random.nextInt(3)+1;
+
+        switch (option){
+            case 1:
+                System.out.println("Option: No Play. The player stays at position : "+ position);
+                break;
+            case 2:
+                position += rollDie;
+                System.out.println("Option: Ladder! The player moves ahead to position " + position);
+                break;
+            case 3:
+                position -= rollDie;
+                if (position < 0) position = 0;
+                System.out.println("Option: Snake! The player moves back to position " + position);
+                break;
+            default:
+                System.out.println("Invalid..!");
+        }
     }
 }
